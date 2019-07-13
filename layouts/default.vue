5e08fb26459f23de01ce73d9ed2bf55abbd382ea
vadim <vadimgordenya@gmail.com>
2019-07-13 19:23:40 +0300

    starting use "create-nuxt-app" with creating chat-nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app app>
+    <h1>Title</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ server/app.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('IO connection');
+});
+
+module.exports = {
+  app,
+  server
+}
+
+
